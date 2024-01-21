fdfb0edbe9efb312879c964159f0b546407377c8
Duk Dara <aramb4131@gmail.com>
2024-01-21 23:38:25 +0700

    first ching

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -32,8 +32,8 @@
             this.label2 = new System.Windows.Forms.Label();
             this.textBox1 = new System.Windows.Forms.TextBox();
             this.textBox2 = new System.Windows.Forms.TextBox();
-            this.button2 = new System.Windows.Forms.Button();
-            this.button3 = new System.Windows.Forms.Button();
+            this.btnCalculateLetterGrade = new System.Windows.Forms.Button();
+            this.btnExit = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // label1
@@ -68,41 +68,42 @@
             // 
             this.textBox2.Location = new System.Drawing.Point(230, 181);
             this.textBox2.Name = "textBox2";
+            this.textBox2.ReadOnly = true;
             this.textBox2.Size = new System.Drawing.Size(168, 26);
             this.textBox2.TabIndex = 3;
             this.textBox2.TextChanged += new System.EventHandler(this.textBox2_TextChanged);
             // 
-            // button2
+            // btnCalculateLetterGrade
             // 
-            this.button2.BackColor = System.Drawing.Color.Chocolate;
-            this.button2.ForeColor = System.Drawing.SystemColors.ControlLightLight;
-            this.button2.Location = new System.Drawing.Point(65, 253);
-            this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(147, 76);
-            this.button2.TabIndex = 5;
-            this.button2.Text = "C&alculate Letter Grade";
-            this.button2.UseVisualStyleBackColor = false;
-            this.button2.Click += new System.EventHandler(this.button2_Click);
+            this.btnCalculateLetterGrade.BackColor = System.Drawing.Color.Snow;
+            this.btnCalculateLetterGrade.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.btnCalculateLetterGrade.Location = new System.Drawing.Point(65, 253);
+            this.btnCalculateLetterGrade.Name = "btnCalculateLetterGrade";
+            this.btnCalculateLetterGrade.Size = new System.Drawing.Size(147, 76);
+            this.btnCalculateLetterGrade.TabIndex = 5;
+            this.btnCalculateLetterGrade.Text = "C&alculate Letter Grade";
+            this.btnCalculateLetterGrade.UseVisualStyleBackColor = false;
+            this.btnCalculateLetterGrade.Click += new System.EventHandler(this.button2_Click);
             // 
-            // button3
+            // btnExit
             // 
-            this.button3.BackColor = System.Drawing.Color.Chocolate;
-            this.button3.ForeColor = System.Drawing.SystemColors.ControlLightLight;
-            this.button3.Location = new System.Drawing.Point(268, 253);
-            this.button3.Name = "button3";
-            this.button3.Size = new System.Drawing.Size(147, 76);
-            this.button3.TabIndex = 6;
-            this.button3.Text = "E&xit";
-            this.button3.UseVisualStyleBackColor = false;
-            this.button3.Click += new System.EventHandler(this.button3_Click);
+            this.btnExit.BackColor = System.Drawing.Color.Snow;
+            this.btnExit.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.btnExit.Location = new System.Drawing.Point(268, 253);
+            this.btnExit.Name = "btnExit";
+            this.btnExit.Size = new System.Drawing.Size(147, 76);
+            this.btnExit.TabIndex = 6;
+            this.btnExit.Text = "E&xit";
+            this.btnExit.UseVisualStyleBackColor = false;
+            this.btnExit.Click += new System.EventHandler(this.button3_Click);
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(456, 359);
-            this.Controls.Add(this.button3);
-            this.Controls.Add(this.button2);
+            this.ClientSize = new System.Drawing.Size(456, 344);
+            this.Controls.Add(this.btnExit);
+            this.Controls.Add(this.btnCalculateLetterGrade);
             this.Controls.Add(this.textBox2);
             this.Controls.Add(this.textBox1);
             this.Controls.Add(this.label2);
@@ -122,8 +123,8 @@
         private System.Windows.Forms.Label label2;
         private System.Windows.Forms.TextBox textBox1;
         private System.Windows.Forms.TextBox textBox2;
-        private System.Windows.Forms.Button button2;
-        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button btnCalculateLetterGrade;
+        private System.Windows.Forms.Button btnExit;
     }
 }
 
